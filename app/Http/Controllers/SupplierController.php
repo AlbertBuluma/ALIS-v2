d0374a405f61c1e8ae0f484a7616d9733419e4fc
AlbertBuluma <albert.buluma1@gmail.com>
2020-11-23 17:24:12 +0300

    Use new syntax

--- app/Http/Controllers/SupplierController.php
+++ app/Http/Controllers/SupplierController.php
@@ -1,54 +1,63 @@
 <?php
 
-class SupplierController extends \BaseController {
+namespace App\Http\Controllers;
+
+use App\Models\Supplier;
+use Illuminate\Database\QueryException;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Validator;
+
+class SupplierController extends Controller {
 
 	/**
 	 * Display a listing of the resource.
 	 *
-	 * @return Response
+	 * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\View\View
 	 */
 	public function index()
 	{
 		//
 		 $suppliers = Supplier::orderBy('name', 'ASC')->get();
-		return View::make('supplier.index')->with('suppliers', $suppliers);
+		return view('supplier.index')->with('suppliers', $suppliers);
 	}
 
 	/**
 	 * Show the form for creating a new resource.
 	 *
-	 * @return Response
+	 * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\View\View
 	 */
 	public function create()
 	{
-		return View::make('supplier.create');
+		return view('supplier.create');
 	}
 
-	/**
-	 * Store a newly created resource in storage.
-	 *
-	 * @return Response
-	 */
-	public function store()
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param Request $request
+     * @return \Illuminate\Http\RedirectResponse
+     */
+	public function store(Request $request)
 	{
 		//
 		$rules = array(
 			'name' => 'required|unique:suppliers,name');
-		$validator = Validator::make(Input::all(), $rules);
+		$validator = Validator::make($request->all(), $rules);
 
-		
+
 		if ($validator->fails()) {
-			return Redirect::back()->withErrors($validator);
+			return redirect()->back()->withErrors($validator);
 		} else {
 			// store
 			$supplier = new Supplier;
-			$supplier->name= Input::get('name');
-			$supplier->phone_no= Input::get('phone_no');
-			$supplier->email= Input::get('email');
-			$supplier->physical_address= Input::get('physical_address');
+			$supplier->name= $request->get('name');
+			$supplier->phone_no= $request->get('phone_no');
+			$supplier->email= $request->get('email');
+			$supplier->physical_address= $request->get('physical_address');
 			try{
 				$supplier->save();
-				return Redirect::route('supplier.index')
+				return redirect('supplier.index')
 					->with('message',  'Successifully added a new supplier');
 			}catch(QueryException $e){
 				Log::error($e);
@@ -73,49 +82,50 @@ class SupplierController extends \BaseController {
 	 * Show the form for editing the specified resource.
 	 *
 	 * @param  int  $id
-	 * @return Response
+	 * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\View\View
 	 */
 	public function edit($id)
 	{
 		$suppliers = Supplier::find($id);
 
 		//Open the Edit View and pass to it the $patient
-		return View::make('supplier.edit')->with('suppliers', $suppliers);
+		return view('supplier.edit')->with('suppliers', $suppliers);
 	}
 
 
-	/**
-	 * Update the specified resource in storage.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function update($id)
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param Request $request
+     * @param int $id
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
+     */
+	public function update(Request $request, $id)
 	{//Validate
 		$rules = array('name' => 'required');
-		$validator = Validator::make(Input::all(), $rules);
+		$validator = Validator::make($request->all(), $rules);
 
 		// process the login
 		if ($validator->fails()) {
-			return Redirect::to('supplier.edit')->withErrors($validator)->withInput(Input::except('password'));
+			return Redirect::to('supplier.edit')->withErrors($validator)->withInput($request->except('password'));
 		} else {
 		// Update
 			$supplier = Supplier::find($id);
-			$supplier->name= Input::get('name');
-			$supplier->physical_address= Input::get('physical_address');
-			$supplier->phone_no= Input::get('phone_no');
-			$supplier->email= Input::get('email');
+			$supplier->name= $request->get('name');
+			$supplier->physical_address= $request->get('physical_address');
+			$supplier->phone_no= $request->get('phone_no');
+			$supplier->email= $request->get('email');
 			$supplier->save();
 			try{
 				$supplier->save();
-				return Redirect::route('supplier.index')
+				return redirect('supplier.index')
 				->with('message', trans('messages.success-updating-supplier')) ->with('activesupplier', $supplier ->id);
 			}catch(QueryException $e){
 				Log::error($e);
 			}
 		}
-			
-		}	
+
+		}
 	/**
 	 * Remove the specified resource from storage.
 	 *
