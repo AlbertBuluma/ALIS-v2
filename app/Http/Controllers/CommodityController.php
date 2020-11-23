c8c4af45cd15fe15b1071daffa9e37f7428a0dff
AlbertBuluma <albert.buluma1@gmail.com>
2020-11-23 17:14:14 +0300

    Use new syntax

--- app/Http/Controllers/CommodityController.php
+++ app/Http/Controllers/CommodityController.php
@@ -4,28 +4,32 @@ namespace App\Http\Controllers;
 
 use App\Models\Commodity;
 use App\Models\Metric;
+use Illuminate\Database\QueryException;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Validator;
 
 class CommodityController extends Controller {
 
 	/**
 	 * Display a listing of the resource.
 	 *
-	 * @return Response
+	 * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\View\View
 	 */
 	public function index()
 	{
 		$commodities = Commodity::all();
-		return View::make('commodity.index')->with('commodities', $commodities);
+		return view('commodity.index')->with('commodities', $commodities);
 	}
 
 
 	public function create()
 	{
-		$metrics = Metric::orderBy('name', 'ASC')->lists('name', 'id');
-		return View::make('commodity.create')->with('metrics', $metrics);
+		$metrics = Metric::orderBy('name', 'ASC')->pluck('name', 'id')->toArray();
+		return view('commodity.create')->with('metrics', $metrics);
 	}
 
-	public function store()
+	public function store(Request $request)
 	{
 		//
 		$rules = array(
@@ -36,25 +40,25 @@ class CommodityController extends Controller {
 		'storage_req' => 'required',
 		'min_level' => 'required|numeric',
 		'max_level' => 'required|numeric',);
-		$validator = Validator::make(Input::all(), $rules);
+		$validator = Validator::make($request->all(), $rules);
 
 		if ($validator->fails()) {
-			return Redirect::back()->withErrors($validator);
+			return redirect()->back()->withErrors($validator);
 		} else {
 			// store
 			$commodity = new Commodity;
-			$commodity->name= Input::get('name');
-			$commodity->description= Input::get('description');
-			$commodity->metric_id= Input::get('unit_of_issue');
-			$commodity->unit_price= Input::get('unit_price');
-			$commodity->item_code = Input::get('item_code');
-			$commodity->storage_req = Input::get('storage_req');
-			$commodity->min_level = Input::get('min_level');
-			$commodity->max_level = Input::get('max_level');
+			$commodity->name= $request->get('name');
+			$commodity->description= $request->get('description');
+			$commodity->metric_id= $request->get('unit_of_issue');
+			$commodity->unit_price= $request->get('unit_price');
+			$commodity->item_code = $request->get('item_code');
+			$commodity->storage_req = $request->get('storage_req');
+			$commodity->min_level = $request->get('min_level');
+			$commodity->max_level = $request->get('max_level');
 
 			try{
 				$commodity->save();
-				return Redirect::route('commodity.index')
+				return redirect('commodity.index')
 					->with('message', trans('messages.commodity-succesfully-added'));
 			}catch(QueryException $e){
 				Log::error($e);
@@ -79,7 +83,7 @@ class CommodityController extends Controller {
 	 * Show the form for editing the specified resource.
 	 *
 	 * @param  int  $id
-	 * @return Response
+	 * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\View\View
 	 */
 	public function edit($id)
 	{
@@ -87,37 +91,38 @@ class CommodityController extends Controller {
 		$commodity = Commodity::find($id);
 
 		//Open the Edit View and pass to it the $patient
-		return View::make('commodity.edit')->with('metrics', $metrics)->with('commodity', $commodity);
+		return view('commodity.edit')->with('metrics', $metrics)->with('commodity', $commodity);
 	}
 
-	/**
-	 * Update the specified resource in storage.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function update($id)
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param Request $request
+     * @param int $id
+     * @return \Illuminate\Http\RedirectResponse
+     */
+	public function update(Request $request, $id)
 	{
 		//Validate
 		$rules = array(
 		'name' => 'required',
 );
-		$validator = Validator::make(Input::all(), $rules);
+		$validator = Validator::make($request->all(), $rules);
 
 		// process the validation
 		if ($validator->fails()) {
-			return Redirect::back()->withErrors($validator)->withInput(Input::except('password'));
+			return redirect()->back()->withErrors($validator)->withInput($request->except('password'));
 		} else {
 		// Update
 			$commodity = Commodity::find($id);
-			$commodity->name= Input::get('name');
-			$commodity->description= Input::get('description');
-			$commodity->metric_id= Input::get('unit_of_issue');
-			$commodity->unit_price= Input::get('unit_price');
-			$commodity->item_code= Input::get('item_code');
-			$commodity->storage_req= Input::get('storage_req');
-			$commodity->min_level= Input::get('min_level');
-			$commodity->max_level= Input::get('max_level');
+			$commodity->name= $request->get('name');
+			$commodity->description= $request->get('description');
+			$commodity->metric_id= $request->get('unit_of_issue');
+			$commodity->unit_price= $request->get('unit_price');
+			$commodity->item_code= $request->get('item_code');
+			$commodity->storage_req= $request->get('storage_req');
+			$commodity->min_level= $request->get('min_level');
+			$commodity->max_level= $request->get('max_level');
 
 
 		try{
--- resources/views/commodity/edit.blade.php
+++ resources/views/commodity/edit.blade.php
@@ -2,9 +2,9 @@
 @section("content")
     <div>
     	<ol class="breadcrumb">
-    	  <li><a href="{{{URL::route('user.home')}}}">{{trans('messages.home')}}</a></li>
-          <li><a href="{{{URL::route('commodity.index')}}}">{{trans('messages.commodityList')}}</a></li>
-    	 	<li class="active">{{ Lang::choice('messages.editCommodities',2) }}</li> 
+    	  <li><a href="{{ route('user.home')}}">{{trans('messages.home')}}</a></li>
+          <li><a href="{{ route('commodity.index') }}">{{trans('messages.commodityList')}}</a></li>
+    	 	<li class="active">{{ Lang::choice('messages.editCommodities',2) }}</li>
     	</ol>
     </div>
 @if (Session::has('message'))
@@ -25,11 +25,11 @@
                'id' => 'form-edit-commodity')) }}
             <div class="form-group">
                 {{ Form::label('name', Lang::choice('messages.name', 2)) }}
-                {{ Form::text('name', Input::old('name'), array('class' => 'form-control', 'rows' => '2')) }}
+                {{ Form::text('name', old('name'), array('class' => 'form-control', 'rows' => '2')) }}
             </div>
              <div class="form-group">
                 {{ Form::label('description', trans('messages.description')) }}
-                {{ Form::textarea('description', Input::old('description'),array('class' => 'form-control', 'rows' => '2')) }}
+                {{ Form::textarea('description', old('description'),array('class' => 'form-control', 'rows' => '2')) }}
             </div>
              <div class="form-group">
                 {{ Form::label('unit_of_issue', trans('messages.unit-of-issue')) }}
@@ -38,27 +38,27 @@
             </div>
             <div class="form-group">
                 {{ Form::label('unit_price', trans('messages.unit-price')) }}
-                {{ Form::text('unit_price', Input::old('unit_price'),array('class' => 'form-control', 'rows' => '2')) }}
+                {{ Form::text('unit_price', old('unit_price'),array('class' => 'form-control', 'rows' => '2')) }}
             </div>
             <div class="form-group">
                 {{ Form::label('item_code', trans('messages.item-code')) }}
-                {{ Form::text('item_code', Input::old('item_code'),array('class' => 'form-control', 'rows' => '2')) }}
+                {{ Form::text('item_code', old('item_code'),array('class' => 'form-control', 'rows' => '2')) }}
             </div>
             <div class="form-group">
                 {{ Form::label('storage_req', trans('messages.storage-req')) }}
-                {{ Form::textarea('storage_req', Input::old('storage_req'),array('class' => 'form-control', 'rows' => '2')) }}
+                {{ Form::textarea('storage_req', old('storage_req'),array('class' => 'form-control', 'rows' => '2')) }}
             </div>
             <div class="form-group">
                 {{ Form::label('min_level ', trans('messages.min-level')) }}
-                {{ Form::text('min_level', Input::old('min_level'),array('class' => 'form-control', 'rows' => '2')) }}
+                {{ Form::text('min_level', old('min_level'),array('class' => 'form-control', 'rows' => '2')) }}
             </div>
             <div class="form-group">
                 {{ Form::label('max_level ', trans('messages.max-level')) }}
-                {{ Form::text('max_level', Input::old('max_level'),array('class' => 'form-control', 'rows' => '2')) }}
+                {{ Form::text('max_level', old('max_level'),array('class' => 'form-control', 'rows' => '2')) }}
             </div>
 
             <div class="form-group actions-row">
-                    {{ Form::button("<span class='glyphicon glyphicon-save'></span> ".trans('messages.save'), 
+                    {{ Form::button("<span class='glyphicon glyphicon-save'></span> ".trans('messages.save'),
                         array('class' => 'btn btn-primary', 'onclick' => 'submit()')) }}
             </div>
         {{ Form::close() }}
