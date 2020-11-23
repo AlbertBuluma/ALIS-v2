5cc16af82bbd26839d3dda318f513125be081078
AlbertBuluma <albert.buluma1@gmail.com>
2020-11-23 10:21:09 +0300

    Use new blade directive syntax

--- resources/views/role/create.blade.php
+++ resources/views/role/create.blade.php
@@ -2,9 +2,9 @@
 @section("content")
 <div>
     <ol class="breadcrumb">
-      <li><a href="{{{URL::route('user.home')}}}">{{trans('messages.home')}}</a></li>
+      <li><a href="{{ route('user.home') }}">{{trans('messages.home')}}</a></li>
       <li>
-        <a href="{{ URL::route('role.index') }}">{{ Lang::choice('messages.role',1) }}</a>
+        <a href="{{ route('role.index') }}">{{ Lang::choice('messages.role',1) }}</a>
       </li>
       <li class="active">{{trans('messages.new-role')}}</li>
     </ol>
@@ -26,15 +26,15 @@
 
             <div class="form-group">
                 {{ Form::label('name', Lang::choice('messages.name',1)) }}
-                {{ Form::text('name', Input::old('name'), array('class' => 'form-control')) }}
+                {{ Form::text('name', old('name'), array('class' => 'form-control')) }}
             </div>
             <div class="form-group">
                 {{ Form::label('description', trans('messages.description')) }}
-                {{ Form::textarea('description', Input::old('description'), 
+                {{ Form::textarea('description', old('description'),
                     array('class' => 'form-control', 'rows' => '2')) }}
             </div>
             <div class="form-group actions-row">
-                    {{ Form::button("<span class='glyphicon glyphicon-save'></span> ".trans('messages.save'), 
+                    {{ Form::button("<span class='glyphicon glyphicon-save'></span> ".trans('messages.save'),
                         array('class' => 'btn btn-primary', 'onclick' => 'submit()')) }}
             </div>
         {{ Form::close() }}
