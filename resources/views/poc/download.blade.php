d86aa8722a48e196fafa9fa60ee9de549a9dcced
AlbertBuluma <albert.buluma1@gmail.com>
2020-11-23 13:50:19 +0300

    Use new blade syntax

--- resources/views/poc/download.blade.php
+++ resources/views/poc/download.blade.php
@@ -2,9 +2,9 @@
 @section("content")
 <div>
 	<ol class="breadcrumb">
-		<li><a href="{{{URL::route('user.home')}}}">{{ trans('messages.home') }}</a></li>
-		<li><a href="{{ URL::route('poc.index') }}">EID Patient list</a></li>
-		<!-- <li><a href="{{ URL::route('bbincidence.bbfacilityreport') }}">Facility Report</a></li> -->
+		<li><a href="{{ route('user.home') }}">{{ trans('messages.home') }}</a></li>
+		<li><a href="{{ route('poc.index') }}">EID Patient list</a></li>
+		<!-- <li><a href="{{ route('bbincidence.bbfacilityreport') }}">Facility Report</a></li> -->
 		<li class="active">Download</li>
 	</ol>
 </div>
@@ -14,20 +14,20 @@
 	</div>
 	<div class="panel-body">
 		<form method="get" action="/poc_download/">
-			
+
 			<div class="row">
 				<div class="col-sm-3 ">
 					<div class='form-group'>
 						<label for="fro">From: </label>
-						{{ Form::text('test_date_fro', Input::old('test_date_fro'), array('class' => 'form-control input-sm standard-datepicker standard-datepicker-nofuture')) }}
+						{{ Form::text('test_date_fro', old('test_date_fro'), array('class' => 'form-control input-sm standard-datepicker standard-datepicker-nofuture')) }}
 					</div>
 					<div class='form-group'>
 						<label for="fro">To: </label>
-						{{ Form::text('test_date_to', Input::old('test_date_to'), array('class' => 'form-control input-sm standard-datepicker standard-datepicker-nofuture')) }}
+						{{ Form::text('test_date_to', old('test_date_to'), array('class' => 'form-control input-sm standard-datepicker standard-datepicker-nofuture')) }}
 					</div>
 					<input type='submit' value='Generate CSV' class="btn btn-primary">
 				</div>
-			</div>				
+			</div>
 		</form>
 
 	</div>
