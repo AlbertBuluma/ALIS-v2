54558424c636990ebc75036a42539ab13cd8bfe6
AlbertBuluma <albert.buluma1@gmail.com>
2020-11-23 10:21:31 +0300

    Use new blade directive syntax

--- resources/views/unhls_patient/create.blade.php
+++ resources/views/unhls_patient/create.blade.php
@@ -2,8 +2,8 @@
 @section("content")
 	<div>
 		<ol class="breadcrumb">
-		  <li><a href="{{{URL::route('user.home')}}}">{{ trans('messages.home') }}</a></li>
-		  <li><a href="{{ URL::route('unhls_patient.index') }}">{{ Lang::choice('messages.patient',2) }}</a></li>
+		  <li><a href="{{ route('user.home')}}">{{ trans('messages.home') }}</a></li>
+		  <li><a href="{{ route('unhls_patient.index') }}">{{ Lang::choice('messages.patient',2) }}</a></li>
 		  <li class="active">{{trans('messages.create-patient')}}</li>
 		</ol>
 	</div>
