e3effc20d0973665b42a4ec2c9ab2aa226ee6c7c
AlbertBuluma <albert.buluma1@gmail.com>
2020-11-24 21:26:47 +0300

    Add model import path

--- resources/views/reports/infection/index.blade.php
+++ resources/views/reports/infection/index.blade.php
@@ -2,7 +2,7 @@
 @section("content")
 <div>
 	<ol class="breadcrumb">
-	  <li><a href="{{{URL::route('user.home')}}}">{{ trans('messages.home') }}</a></li>
+	  <li><a href="{{ route('user.home') }}">{{ trans('messages.home') }}</a></li>
 	  <li class="active">{{ Lang::choice('messages.report',2) }}</li>
 	  <li class="active">{{ trans('messages.infection-report') }}</li>
 	</ol>
@@ -16,7 +16,7 @@
 					{{ Form::label('start', trans("messages.from")) }}
 				</div>
 				<div class="col-md-10">
-					{{ Form::text('start', isset($input['start'])?$input['start']:date('Y-m-d'), 
+					{{ Form::text('start', isset($input['start'])?$input['start']:date('Y-m-d'),
 				        array('class' => 'form-control standard-datepicker')) }}
 			    </div>
 	    	</div>
@@ -27,7 +27,7 @@
 			    	{{ Form::label('end', trans("messages.to")) }}
 			    </div>
 				<div class="col-md-10">
-				    {{ Form::text('end', isset($input['end'])?$input['end']:date('Y-m-d'), 
+				    {{ Form::text('end', isset($input['end'])?$input['end']:date('Y-m-d'),
 				        array('class' => 'form-control standard-datepicker')) }}
 		        </div>
 	    	</div>
@@ -37,12 +37,12 @@
 	        	{{ Form::label('test_type', Lang::choice('messages.test-category',1)) }}
 	        </div>
 	        <div class="col-md-8">
-	            {{ Form::select('test_category', array(0 => '-- All --')+TestCategory::all()->sortBy('name')->lists('name','id'),
+	            {{ Form::select('test_category', array(0 => '-- All --')+ App\Models\TestCategory::all()->sortBy('name')->pluck('name','id')->toArray(),
 	            	isset($input['test_category'])?$input['test_category']:0, array('class' => 'form-control')) }}
 	        </div>
         </div>
 	    <div class="col-md-2">
-		    {{ Form::button("<span class='glyphicon glyphicon-filter'></span> ".trans('messages.view'), 
+		    {{ Form::button("<span class='glyphicon glyphicon-filter'></span> ".trans('messages.view'),
 		        array('class' => 'btn btn-info', 'id' => 'filter', 'type' => 'submit')) }}
 	    </div>
 	</div>
@@ -57,9 +57,9 @@
 	<div class="panel-body">
 	@if (Session::has('message'))
 		<div class="alert alert-info">{{ trans(Session::get('message')) }}</div>
-	@endif	
+	@endif
 	<strong>
-		<p> {{ trans('messages.infection-report') }} - 
+		<p> {{ trans('messages.infection-report') }} -
 			<?php $from = isset($input['start'])?$input['start']:date('01-m-Y');?>
 			<?php $to = isset($input['end'])?$input['end']:date('d-m-Y');?>
 			@if($from!=$to)
@@ -89,7 +89,7 @@
 					</tr>
 				</thead>
 				<tbody>
-					<?php 
+					<?php
 						$testRow = "";
 
 						$currentTest = "";
