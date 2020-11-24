47669e9f1d5cd53db37e6abc4884fc5f089e42de
AlbertBuluma <albert.buluma1@gmail.com>
2020-11-24 11:01:15 +0300

    Add machine interfacing endpoints

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,34 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::group(array("before" => "guest"), function()
+{
+    /*
+    |-----------------------------------------
+    | API route
+    |-----------------------------------------
+    | Proposed route for the BLIS api, we will receive api calls
+    | from other systems from this route.
+    */
+    Route::post('/api/receiver', array(
+        "as" => "api.receiver",
+        "uses" => "InterfacerController@receiveLabRequest"
+    ));
+    Route::post('/api/testinfo', array(
+        "uses" => "InterfacerController@getTestInfo"
+    ));
+    Route::post('/api/searchtests', array(
+        "uses" => "InterfacerController@getTests"
+    ));
+    Route::post('/api/saveresults', array(
+        "uses" => "InterfacerController@saveTestResults"
+    ));
+    Route::any('/', array(
+        "as" => "user.login",
+        "uses" => "UserController@loginAction"
+    ));
+
+});
+
