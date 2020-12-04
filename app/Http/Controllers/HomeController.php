0006ea345f38f9b491f6ae30948d15447b51807c
AlbertBuluma <albert.buluma1@gmail.com>
2020-12-04 09:51:59 +0300

    Add Auth middleware and define logout redirect route

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,23 +1,40 @@
 <?php
 
-class HomeController extends BaseController {
+namespace App\Http\Controllers;
 
-	/*
-	|--------------------------------------------------------------------------
-	| Default Home Controller
-	|--------------------------------------------------------------------------
-	|
-	| You may wish to use controllers instead of, or in addition to, Closure
-	| based routes. That's great! Here is an example controller method to
-	| get you started. To route to this controller, just add the route:
-	|
-	|	Route::get('/', 'HomeController@showWelcome');
-	|
-	*/
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
-	public function showWelcome()
-	{
-		return View::make('hello');
-	}
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
 
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        return view('user.home');
+    }
+
+    public function logout(Request $request)
+    {
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        Auth::logout();
+
+        return redirect('/');
+    }
 }
