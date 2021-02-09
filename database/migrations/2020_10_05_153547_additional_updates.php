35df3bcf3af5d375f7e9fac1bf2b64099ce23adb
AlbertBuluma <albert.buluma1@gmail.com>
2021-02-09 15:19:13 +0300

    Modify POC result table columns

--- database/migrations/2020_10_05_153547_additional_updates.php
+++ database/migrations/2020_10_05_153547_additional_updates.php
@@ -26,20 +26,23 @@ class AdditionalUpdates extends Migration {
 		DB::update('ALTER TABLE unhls_test_results ADD time_revised DATE NULL AFTER revised_by');
 		// DB::update('ALTER TABLE referrals ADD test_id INT(10) NULL AFTER status');
 		DB::update('ALTER TABLE unhls_tests  ADD instrument_id INT  AFTER instrument,  ADD method_used VARCHAR(60) NULL');
-        DB::statement('ALTER TABLE `unhls_tests` CHANGE `purpose` `purpose` VARCHAR(10) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL;');
+        DB::statement('ALTER TABLE `unhls_tests` CHANGE `purpose` `purpose` VARCHAR(10) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL');
 		DB::update('ALTER TABLE unhls_visits ADD is_printed INT(2) NOT NULL DEFAULT 0 AFTER facility_lab_number');
 		DB::update('ALTER TABLE unhls_visits ADD printed_by INT(3) NULL AFTER is_printed, ADD time_printed DATETIME NULL AFTER printed_by');
 		DB::update('ALTER TABLE test_types ADD parentId INT(3) NULL AFTER description');
 		DB::update('ALTER TABLE test_name_mappings ADD test_category_id INT(10) NULL AFTER test_type_id');
 		DB::update('ALTER TABLE specimens ADD sample_obtainer VARCHAR(60) NULL DEFAULT NULL AFTER accepted_by');
         DB::update('ALTER TABLE clinicians ADD active INT(3) NOT NULL DEFAULT 0 AFTER email');
-        DB::statement('ALTER TABLE `clinicians` CHANGE `email` `email` VARCHAR(191) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL;');
+        DB::statement('ALTER TABLE `clinicians` CHANGE `email` `email` VARCHAR(191) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL');
         DB::insert('INSERT INTO `model_has_permissions` (`permission_id`, `model_type`, `model_id`) VALUES (18, "User", 1)');
         DB::statement('ALTER TABLE `poc_tables` ADD `mother_hts` VARCHAR(60) NULL DEFAULT NULL AFTER `mother_name`');
         DB::statement('ALTER TABLE `poc_tables` ADD `mother_art` VARCHAR(60) NULL DEFAULT NULL AFTER `mother_hts`');
+        DB::statement('ALTER TABLE `poc_tables` CHANGE `mother_hiv_status` `mother_hiv_status` VARCHAR(191) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL');
         DB::statement('ALTER TABLE `poc_tables` CHANGE `admission_date` `admission_date` DATE NULL DEFAULT NULL');
         DB::statement('ALTER TABLE `poc_tables` CHANGE `mother_name` `mother_name` VARCHAR(191) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL');
         DB::statement('ALTER TABLE `poc_tables` CHANGE `provisional_diagnosis` `provisional_diagnosis` VARCHAR(191) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL');
+        DB::statement('ALTER TABLE `poc_results` CHANGE `test_time` `test_time` TIME NULL DEFAULT NULL');
+        DB::statement('ALTER TABLE `poc_tables` CHANGE `caretaker_number` `caretaker_number` VARCHAR(191) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL');
     }
 
 	/**
